870dc17ac14c42b98325a10f73f1fd81b5b88871
Raya-Alyoubi <raya.rm480@gmail.com>
2026-03-31 16:11:54 +0300

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/TheMet-Metropolitan_Museum_Explorer/",
+  base: "/CPIT-405-Group_Project/",
 });
