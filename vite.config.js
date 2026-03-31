a3b08aaabccf49c230693799d08bfbd5beedfb92
Raya-Alyoubi <Raya.alyoubi@outlook.com>
2026-03-31 17:17:15 +0300

    Deploy site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CPIT-405-Group_Project/",
+  base: '/TheMet-Metropolitan_Museum_Explorer/',
+  build: {
+    outDir:'docs'
+  }
 });
